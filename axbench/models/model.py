29b1cf9bbabe83deb6e2a3860d486e1a55853db3
frankaging <zxw0424@gmail.com>
2024-10-24 21:53:04 -0700

    [Minor] Fixing probe, add L1 probe

--- axbench/models/model.py
+++ axbench/models/model.py
@@ -3,6 +3,13 @@ import torch
 
 class Model(ABC):
 
+    def __init__(self, model, tokenizer, layer, training_args=None, **kwargs):
+        self.model = model
+        self.tokenizer = tokenizer
+        # abstracting layer
+        self.layer = layer
+        self.training_args = training_args
+
     def make_model(self, **kwargs):
         pass
 
@@ -15,13 +22,13 @@ class Model(ABC):
         
     def save(self, dump_dir, **kwargs):
         weight_file = dump_dir / f"{self.__str__()}_weight.pt"
-        weight = self.linear_model.proj.weight.data.cpu()
+        weight = self.ax.proj.weight.data.cpu()
         if weight_file.exists():
             weight = torch.cat([torch.load(weight_file), weight], dim=0)
         torch.save(weight, weight_file)
         
         bias_file = dump_dir / f"{self.__str__()}_bias.pt"
-        bias = self.linear_model.proj.bias.data.cpu()
+        bias = self.ax.proj.bias.data.cpu()
         if bias_file.exists():
             bias = torch.cat([torch.load(bias_file), bias], dim=0)
         torch.save(bias, bias_file)
@@ -33,8 +40,9 @@ class Model(ABC):
         bias = torch.load(
             f"{dump_dir}/{self.__str__()}_bias.pt"
         )
-        self.linear_model.proj.weight.data = weight.cuda()
-        self.linear_model.proj.bias.data = bias.cuda()
+        self.make_model(low_rank_dimension=weight.shape[1])
+        self.ax.proj.weight.data = weight.cuda()
+        self.ax.proj.bias.data = bias.cuda()
     
     def predict_latent(self, examples, **kwargs):
         pass
